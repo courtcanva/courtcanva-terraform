3705faec0e768f24457732ce81fe9786a1cd3e78
KikiPang <75398402+ki-ki-316@users.noreply.github.com>
2022-06-04 17:33:13 +1000

    Add files via upload

---
+++ Jenkins-EC2/terraform.tfvars
@@ -0,0 +1,5 @@
+jenkins_alb_name = ""
+
+jenkins_cert_domain_name = ""
+
+aw_ami = "ami-0b21dcff37a8cd8a4"
---
+++ Jenkins-EC2/variables.tf
@@ -0,0 +1,13 @@
+variable "jenkins_alb_name" {
+  description = "The name for Jenkins ALB"
+}
+
+variable "jenkins_cert_domain_name" {
+  description = "The name for Jenkins SSL cert domain"
+}
+
+variable "aw_ami" {
+  description = "instance ami"
+}
+
+
