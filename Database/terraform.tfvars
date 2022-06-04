603b6fa4a6d226ba309c69e2e08d7154e104a421
KikiPang <75398402+ki-ki-316@users.noreply.github.com>
2022-06-04 17:34:01 +1000

    Add files via upload

---
+++ Database/terraform.tfvars
@@ -0,0 +1,25 @@
+public_key = "Qvknifei"
+
+private_key = "36e1d19c-5577-483e-ab83-65e8d539781d"
+
+atlas_region = "ap-southeast-2"
+
+aws_region = "ap-southeast-2"
+ 
+atlas_dbuser = "DBUATtest"
+
+atlas_dbpassword = "123456!!"
+
+aws_account_id = "497551902879"
+
+atlasorgid = "6280aadbe8b5363a7e65adf5"
+  
+atlas_vpc_cidr = "192.168.128.0/21"
+
+mongodbprojectname = "ccuatDB"
+
+clustername = "ccuatcluster"
+environment = ""
+dbvpc = ""
+dbsubnet = ""
+ECSNatGatewayEIP = ""
---
+++ Database/variables.tf
@@ -0,0 +1,56 @@
+variable "public_key" {
+  description = "The public API key for MongoDB Atlas"
+}
+variable "private_key" {
+  description = "The private API key for MongoDB Atlas"
+}
+
+/*variable "access_key" {
+  description = "The access key for AWS Account"
+}
+variable "secret_key" {
+  description = "The secret key for AWS Account"
+}*/
+variable "atlas_region" {
+  default     = "ap-southeast-2"
+  description = "Atlas Region"
+}
+variable "aws_region" {
+  default     = "ap-southeast-2"
+  description = "AWS Region"
+}
+variable "atlas_dbuser" {
+  description = "The db user for Atlas"
+}
+variable "atlas_dbpassword" {
+  description = "The db user passwd for Atlas"
+}
+variable "aws_account_id" {
+  description = "My AWS Account ID"
+  
+}
+variable "atlasorgid" {
+  description = "Atlas Org ID"
+  
+}
+variable "atlasprojectid" {
+  description = "Atlas Project ID"
+  
+}
+variable "atlas_vpc_cidr" {
+  description = "Atlas CIDR"
+  
+}
+variable "mongodbprojectname" {
+  description = "DB Name"
+
+}
+variable "clustername" {
+  description = "Cluster Name"
+  
+}
+
+variable "environment" {
+  type        = string
+  description = "The environment (uat, prod, test, etc.)  for the website."
+}
