72145d19fb68a0724fed6d6e5521ab04ad460714
KikiPang <75398402+ki-ki-316@users.noreply.github.com>
2022-06-05 20:51:04 +1000

    Update jenkins_init.sh

--- Jenkins-EC2/jenkins_init.sh
+++ Jenkins-EC2/jenkins_init.sh
@@ -27,4 +27,34 @@ curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt install -y terraform
 
-# install packer
+sudo su - 
+mkdir /var/jenkins_home
+sudo apt update -y
+sudo apt install apache2 -y
+sudo systemctl start apache2
+cd /var/www/html
+echo "<html><h1> Jenkins is healthy</h1></html>" > index.html 
+
+apt install npm -y 
+npm i next -y
+
+#mount ebs
+sudo mkfs -t xfs /dev/nvme1n1  #sudo mkfs -t xfs /dev/xvdh
+sudo mount /dev/nvme1n1 /var/jenkins_home  #sudo mount /dev/xvdh /var/jenkins_home
+
+#update /ets/fstab
+sudo cp /etc/fstab  /etc/fstab.orig
+sudo chmod 666 /etc/fstab
+#sudo echo UUID=$(blkid |grep xvdh |awk -F "\"" '{print $2}') /var/jenkins_home xfs defaults,nofail 0 2 >>/etc/fstab
+sudo echo UUID=$(blkid |grep nvme1n1 |awk -F "\"" '{print $2}') /var/jenkins_home xfs defaults,nofail 0 2 >>/etc/fstab
+sudo chmod 644 /etc/fstab
+#change jenkins home path
+chown jenkins:jenkins /var/jenkins_home
+cp -r /var/lib/jenkins /var/jenkins_home 
+cp * -r .bash_history .cache .groovy .java .lastStarted .viminfo /var/jenkins_home
+usermod -d /var/jenkins_home/ jenkins
+sed -i 's|/var/lib/$NAME|/var/jenkins_home|g' /etc/default/jenkins
+sed -i 's|/var/lib/jenkins|/var/jenkins_home|g' /lib/systemd/system/jenkins.service
+sed -i 's|/var/lib/jenkins|/var/jenkins_home|g' /etc/passwd
+systemctl daemon-reload
+sudo service jenkins restart
