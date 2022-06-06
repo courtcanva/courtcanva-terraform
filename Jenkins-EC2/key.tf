0a6fc92f1a5c9754847887325cc2ab54c1c59fe8
KikiPang <75398402+ki-ki-316@users.noreply.github.com>
2022-06-06 20:02:01 +1000

    Update key.tf

--- Jenkins-EC2/key.tf
+++ Jenkins-EC2/key.tf
@@ -1,4 +1,7 @@
-resource "aws_key_pair" "ssh-key" {
-  key_name   = "mykey"
-  public_key = "${file(var.public_key_location)}"
+data "aws_key_pair" "ssh-key" {
+  key_name = "jenkinsec2keypair"
+  filter {
+    name   = "tag:KeyPair"
+    values = ["JenkinsKey"]
+  }
 }
