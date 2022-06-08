524001a76c49b7dcabd38c7fb7b3481e6421db47
SunnyLLL <92508027+SunnyLLL@users.noreply.github.com>
2022-06-08 11:43:27 +0800

    Update Provider.tf

--- Jenkins-EC2/Provider.tf
+++ Jenkins-EC2/Provider.tf
@@ -1,10 +1,14 @@
 terraform {
-  required_version = ">= 0.12"
-}
+  required_providers {
+    aws = {
+      source ="hashicorp/aws"
+      version = ">=2.7.0"
+    }
+  }
 
 provider "aws" {
-  shared_credentials_file = "$HOME/jenkins/.aws/credentials"
-  profile                 = "default"
+#  shared_credentials_file = "$HOME/jenkins/.aws/credentials"
+#  profile                 = "default"
 
   region = var.aws_region
 default_tags {
