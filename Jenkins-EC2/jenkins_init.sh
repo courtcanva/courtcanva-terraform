d68037b998ab2fe50300e9eac024e69b21749371
SunnyLLL <92508027+SunnyLLL@users.noreply.github.com>
2022-06-09 11:39:14 +0800

    Update jenkins_init.sh

--- Jenkins-EC2/jenkins_init.sh
+++ Jenkins-EC2/jenkins_init.sh
@@ -5,34 +5,36 @@
 sudo su - 
 mkdir /var/jenkins_home
 
+# install jenkins 
 sudo apt-get update
 sudo apt-get install openjdk-8-jdk -y 
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install jenkins -y
-sudo apt install git -y
-sudo apt-get install ca-certificates curl gnupg lsb-release
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-sudo systemctl start docker
-sudo systemctl enable docker 
-sudo docker version
-sudo docker images
 
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-sudo apt-get install nodejs -y 
-sudo apt install awscli -y
+# sudo apt install git -y
+# sudo apt-get install ca-certificates curl gnupg lsb-release
+# sudo mkdir -p /etc/apt/keyrings
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+# echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# sudo apt-get update
+# sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+# sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+# sudo systemctl start docker
+# sudo systemctl enable docker 
+# sudo docker version
+# sudo docker images
 
-sudo apt update -y
-sudo apt install apache2 -y
-sudo systemctl start apache2
-cd /var/www/html
-echo "<html><h1> Jenkins is healthy</h1></html>" > index.html 
+# curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+# sudo apt-get install nodejs -y 
+# sudo apt install awscli -y
+
+# sudo apt update -y
+# sudo apt install apache2 -y
+# sudo systemctl start apache2
+# cd /var/www/html
+# echo "<html><h1> Jenkins is healthy</h1></html>" > index.html 
 
 #mount ebs
 sudo mkfs -t xfs /dev/nvme1n1  #sudo mkfs -t xfs /dev/xvdh
