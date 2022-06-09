fcbd84c11903a496b911da42f73d8dbbf1c47f65
SunnyLLL <92508027+SunnyLLL@users.noreply.github.com>
2022-06-09 15:53:30 +0800

    Update provider.tf

--- Jenkins-EC2-TF/provider.tf
+++ Jenkins-EC2-TF/provider.tf
@@ -11,4 +11,20 @@ provider "aws" {
 
   region  = "ap-southeast-2"   
   #shared_credentials_file = "%USERPROFILE%/.aws/credentials"
+  default_tags {
+    tags = {
+      name = "JenkinsEC2"
+      Project        = "Courtcanva"
+    }
+  }  
+}
+
+terraform {
+  backend  "s3" {
+ key = "uat/jenkins-ec2n/terraform.tfstate"
+ region = "ap-southeast-2"
+ bucket = "cc-terraform-state-file"
+ dynamodb_table = "terraform-state-locking"
+#encrypt = true # Optional, S3 Bucket Server Side Encryption
+ }
 }
