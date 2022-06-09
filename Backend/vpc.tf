168183c40c8880795465c9ea43333271c3982692
SunnyLLL <92508027+SunnyLLL@users.noreply.github.com>
2022-06-09 19:32:01 +0800

    Update vpc.tf

--- Backend/vpc.tf
+++ Backend/vpc.tf
@@ -40,20 +40,20 @@ resource "aws_route" "internet_access" {
 
 # Create a NAT gateway with an Elastic IP for each private subnet to get internet connectivity
 resource "aws_eip" "gw" {
-  count      = var.az_count
+  count      = 1
   vpc        = true
   depends_on = [aws_internet_gateway.gw]
 }
 
 resource "aws_nat_gateway" "gw" {
-  count         = var.az_count
+  count         = 1
   subnet_id     = element(aws_subnet.public.*.id, count.index)
   allocation_id = element(aws_eip.gw.*.id, count.index)
 }
 
 # Create a new route table for the private subnets, make it route non-local traffic through the NAT gateway to the internet
 resource "aws_route_table" "private" {
-  count  = var.az_count
+  count  = 1
   vpc_id = aws_vpc.main.id
 
   route {
@@ -64,7 +64,7 @@ resource "aws_route_table" "private" {
 
 # Explicitly associate the newly created route tables to the private subnets (so they don't default to the main route table)
 resource "aws_route_table_association" "private" {
-  count          = var.az_count
+  count          = 1
   subnet_id      = element(aws_subnet.private.*.id, count.index)
   route_table_id = element(aws_route_table.private.*.id, count.index)
 }
